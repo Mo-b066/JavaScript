5973597cd9e7bd8dc69932de76c8c7c5fa55de07
Mo-b066 <119445295+Mo-b066@users.noreply.github.com>
2025-02-17 18:12:46 +0100

    Begin anlegung Variables

---
+++ Variables/index.js
@@ -0,0 +1,9 @@
+//console.log('hallo');
+//console.log('i like pizza');
+
+//window.alert('hallo');
+//window.alert('hallo bot');
+
+
+document.getElementById("myH1").textContent = 'hallo';
+document.getElementById("myP").textContent = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.';
