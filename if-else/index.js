2fc7a3d906261c3d83f476791ac99a73bbd625a4
Mo-b066 <119445295+Mo-b066@users.noreply.github.com>
2025-02-20 20:16:05 +0100

    if-else

--- if-else/index.js
+++ if-else/index.js
@@ -4,3 +4,18 @@ const output = document.getElementById("output");
 
 let age;
 
+submitBtn.onclick = function(){
+    
+    age = inputText.value;
+    age = Number(age);  
+
+    if(age >= 18){
+        output.textContent = "You are allowed to enter the site";
+    }
+    else if(age < 18){
+        output.textContent = "You are NOT allowed to enter the site";
+    }
+    else{
+        output.textContent = "Enter a valid number";
+    }
+}
